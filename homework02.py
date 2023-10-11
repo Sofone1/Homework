8abf25c595a9777370e1277ad69d55fd8b041fda
Sophio Tevdoradze <tevdoradzesophio@gmail.com>
2023-10-11 23:32:16 +0400

    second commit

---
+++ homework02.py
@@ -0,0 +1,31 @@
+# float:
+x = 2 * (12 + 6) * (34+67) + 12 / (3+6)
+print(x)
+# integer:
+y = 2 * (12 + 6) * (34+67) + 12 // (3+6)
+print(y)
+
+# ((4 + 23)^2 + 56 + 89)(52 + 12)^6
+z = ((4+23)**2 + 56 + 89) * (52 + 12)**6
+print(z)
+
+# radiusi = 5 ___ sigrdze? fartobi?
+n = 3.14 * 5 * 2
+s = 3.14 * 5**2
+print('length =', n, 'area =', s)
+
+# 200000 wami
+d = 200000 // 60
+h = 200000 % 60
+print(d, 'minute', '+', h, 'second')
+
+# S = 150000km  t = 10000h:
+
+v = 150000 / 10000
+print('v =', int(v), 'km/h')
+
+# birthday:
+f = 14
+g = 'May'
+r = 2008
+print(f, g, r)
